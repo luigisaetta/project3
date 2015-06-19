e2e11c99ef7bdf8d454c3d9e503877dc71a360ba
LSaetta <LSaetta@mbp-di-luigi.homenet.telecomitalia.it>
2015-06-19 18:17:01 +0200

    modificato classe1

--- src/saetta/terzo/Classe1.java
+++ src/saetta/terzo/Classe1.java
@@ -6,7 +6,7 @@ public class Classe1
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-        System.out.println("Hello world");
+        System.out.println("Hello world introduco errore");
 	}
 
 }
