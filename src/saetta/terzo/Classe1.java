e55e61b23972b7b1c44f329ab54f0fe092cd376a
LSaetta <LSaetta@mbp-di-luigi.homenet.telecomitalia.it>
2015-06-19 18:15:59 +0200

    aggiunto classe1

---
+++ src/saetta/terzo/Classe1.java
@@ -0,0 +1,12 @@
+package saetta.terzo;
+
+public class Classe1
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+        System.out.println("Hello world");
+	}
+
+}
